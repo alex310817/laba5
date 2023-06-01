614433a94d5bced3620db326f4a73ee42b8cea68
alex <alex310817@gmail.com>
2023-06-01 23:33:57 +0300

    AppDrawer.kt 2

--- app/src/main/java/ui/components/AppDrawer.kt
+++ app/src/main/java/ui/components/AppDrawer.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
@@ -21,6 +22,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.topic2.android.notes.theme.JetNotesTheme
+import com.topic2.android.notes.theme.JetNotesThemeSettings
 import ui.components.AppDrawerHeader
 import com.raywenderlich.android.jetnotes.util.components.ScreenNavigationButton as ScreenNavigationButton
 
@@ -104,6 +106,33 @@ private fun ScreenNavigationButton(
         }
     }
 }
+
+
+@Composable
+private fun LightDarkThemeItem(){
+    Row(
+        Modifier
+            .padding(8.dp)
+    ) {
+        Text(
+            text = "Включить тёмную тему",
+            style = MaterialTheme.typography.body2,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.6f),
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 8.dp, end = 8.dp, top = 8.dp, bottom = 8.dp)
+                .align(alignment = Alignment.CenterVertically)
+        )
+        Switch(
+            checked = JetNotesThemeSettings.isDarkThemeEnabled,
+            onCheckedChange ={ JetNotesThemeSettings.isDarkThemeEnabled = it},
+            modifier = Modifier
+                .padding(start = 8.dp, end = 8.dp)
+                .align(alignment = Alignment.CenterVertically)
+        )
+    }
+}
+
 
 
 
